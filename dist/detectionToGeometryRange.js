36953d4d9d713ae5e758cfef43a487a9def3b9ac
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-08-28 07:43:48 +0200

    remove debugger

--- dist/detectionToGeometryRange.js
+++ dist/detectionToGeometryRange.js
@@ -91,7 +91,6 @@ export function generateGeometriesByNumberOfColors(imageDomId, numberOfColors = 
     if (!image) {
         throw new Error(`Cannot find the element with the id '${imageDomId}'`);
     }
-    debugger
     const palette = computePalette("quantification", {
         image: image,
         numberOfColors
